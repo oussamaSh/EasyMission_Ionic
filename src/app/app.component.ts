29e0d831fc059e8e4f4fdab8b59fff8d5f644984
Ben Youssef Chemseddine <byoussefchems@gmail.com>
2017-12-10 00:12:29 +0100

    structure ionic project with provider , model , page 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { RegisterPage } from './../pages/User/register/register';
+import { LoginPage } from './../pages/User/login/login';
 import { Component, ViewChild } from '@angular/core';
 
 import { Platform, MenuController, Nav } from 'ionic-angular';
 
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -30,7 +31,8 @@ export class MyApp {
     // set our app's pages
     this.pages = [
       { title: 'Hello Ionic', component: HelloIonicPage },
-      { title: 'My First List', component: ListPage }
+      { title: 'Login', component: LoginPage },
+      { title: 'Register', component: RegisterPage },
     ];
   }
 
